73fbe19e063dd0cc8d70f11594321b2a7c6a20d2
mkay <mkay@gmail.com>
2025-05-25 02:09:25 +0300

    sky

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,33 @@
-#!/usr/bin/env python3
+# lib/book.py
 
 class Book:
-    pass
+    def __init__(self, title, page_count, price=0):
+        self.title = title
+        self._page_count = None
+        self._price = None
+        self.page_count = page_count  # use setter for validation
+        self.price = price            # use setter for validation
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price must be non-negative")
+        self._price = value
     
-        
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,32 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size, price=0):
+        self.brand = brand
+        self._size = None
+        self._price = None
+        self.size = size
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price must be non-negative")
+        self._price = value
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
